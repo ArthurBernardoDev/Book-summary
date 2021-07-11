ca20fc70dcd365a003b18f5fbe03c66c022bd9be
Arthur bernardo <arthurutschgg@gmail.com>
2021-07-11 12:42:43 -0300

    base

--- src/pages/habitosAtomicos.tsx
+++ src/pages/habitosAtomicos.tsx
@@ -1,5 +1,24 @@
 export function HabitosAtomicos() {
     return (
-        <h2>ge</h2>
+        <div className="habitosContainer">
+            <h1>Hábitos Atômicos</h1>
+            <h2>Conteúdo do livro</h2>
+
+            <ul>
+                <li>Introdução</li>
+
+                <li><h3>Os fundamentos</h3></li>
+                <li>O supreendente poder dos Hábitos Atômicos</li>
+                <li>Por que seus hábitos molda sua identidade (e Vice-versa)</li>
+
+                <li><h3>A 1lei Torne-o Claro</h3></li>
+
+                <li>O Homem que não parecia bem</li>
+                <li>A melhor maneira de criar um novo hábito</li>
+                <li>A motivação é superestimada; o ambiente é mais relevante</li>
+                <li>O segredo do autocontrole</li>
+
+            </ul>
+        </div>
     )
 }
